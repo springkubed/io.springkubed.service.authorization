357daf3dca66285cd21dadccfeeed3f572e63636
dashaun <dashaun@dashaun.com>
2023-05-01 17:42:00 -0500

    tests are passing, first pass, nothing real yet

---
+++ src/test/java/io/springkubed/service/authorization/authorizationservice/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springkubed.service.authorization.authorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
